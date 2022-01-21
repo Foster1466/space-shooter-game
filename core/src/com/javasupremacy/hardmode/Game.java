66465718bc23e49f04c581c9d9fb7b59eeadf638
Max <kolxy@qq.com>
2022-01-20 23:15:15 -0800

    ball trial

--- core/src/com/javasupremacy/hardmode/Game.java
+++ core/src/com/javasupremacy/hardmode/Game.java
@@ -1,31 +1,38 @@
 package com.javasupremacy.hardmode;
 
+
 import com.badlogic.gdx.ApplicationAdapter;
-import com.badlogic.gdx.graphics.Texture;
-import com.badlogic.gdx.graphics.g2d.SpriteBatch;
-import com.badlogic.gdx.utils.ScreenUtils;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
+import com.badlogic.gdx.graphics.GL20;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
 
 public class Game extends ApplicationAdapter {
-	SpriteBatch batch;
-	Texture img;
-	
+	ShapeRenderer shape;
+	List<Ball> balls = new ArrayList<>();
+	Random r = new Random();
+
 	@Override
 	public void create () {
-		batch = new SpriteBatch();
-		img = new Texture("badlogic.jpg");
+		shape = new ShapeRenderer();
+		for (int i = 0; i < 10; i++) {
+			balls.add(new Ball(r.nextInt(Gdx.graphics.getWidth()),
+					r.nextInt(Gdx.graphics.getHeight()),
+					25, r.nextInt(15), r.nextInt(15)));
+		}
 	}
 
 	@Override
-	public void render () {
-		ScreenUtils.clear(1, 0, 0, 1);
-		batch.begin();
-		batch.draw(img, 0, 0);
-		batch.end();
-	}
-	
-	@Override
-	public void dispose () {
-		batch.dispose();
-		img.dispose();
+	public void render() {
+		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+		shape.begin(ShapeRenderer.ShapeType.Filled);
+		for (Ball ball : balls) {
+			ball.update();
+			ball.draw(shape);
+		}
+		shape.end();
 	}
 }
