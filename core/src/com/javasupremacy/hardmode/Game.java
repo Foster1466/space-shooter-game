f185f89171da94120b086e5c761bd05e5ab2e983
Max <kolxy@qq.com>
2022-02-01 22:23:27 -0800

    movement

--- core/src/com/javasupremacy/hardmode/Game.java
+++ core/src/com/javasupremacy/hardmode/Game.java
@@ -3,6 +3,9 @@ package com.javasupremacy.hardmode;
 
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
 import com.badlogic.gdx.graphics.GL20;
 
@@ -11,28 +14,35 @@ import java.util.List;
 import java.util.Random;
 
 public class Game extends ApplicationAdapter {
-	ShapeRenderer shape;
-	List<Ball> balls = new ArrayList<>();
-	Random r = new Random();
+	SpriteBatch batch;
+	Texture img;
+	float x;
+	float y;
+	float speed = 5;
 
 	@Override
 	public void create () {
-		shape = new ShapeRenderer();
-		for (int i = 0; i < 10; i++) {
-			balls.add(new Ball(r.nextInt(Gdx.graphics.getWidth()),
-					r.nextInt(Gdx.graphics.getHeight()),
-					25, r.nextInt(15), r.nextInt(15)));
-		}
+		batch = new SpriteBatch();
+		img = new Texture("badlogic.jpg");
 	}
 
 	@Override
 	public void render() {
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-		shape.begin(ShapeRenderer.ShapeType.Filled);
-		for (Ball ball : balls) {
-			ball.update();
-			ball.draw(shape);
+		if (Gdx.input.isKeyPressed(Input.Keys.UP)) {
+			y += speed;
 		}
-		shape.end();
+		if (Gdx.input.isKeyPressed(Input.Keys.DOWN)) {
+			y -= speed;
+		}
+		if (Gdx.input.isKeyPressed(Input.Keys.RIGHT)) {
+			x += speed;
+		}
+		if (Gdx.input.isKeyPressed(Input.Keys.LEFT)) {
+			x -= speed;
+		}
+		batch.begin();
+		batch.draw(img, x, y);
+		batch.end();
 	}
 }
