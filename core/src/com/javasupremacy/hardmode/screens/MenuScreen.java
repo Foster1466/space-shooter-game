661058f0563cdcd6e7d9c80929a7848bd0540946
gsingh1 <garhgaj.singh@wsu.edu>
2022-02-04 23:04:05 -0800

    background updated

--- core/src/com/javasupremacy/hardmode/screens/MenuScreen.java
+++ core/src/com/javasupremacy/hardmode/screens/MenuScreen.java
@@ -4,23 +4,41 @@ import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Screen;
 
 import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.javasupremacy.hardmode.MainGame;
+import com.badlogic.gdx.ApplicationListener;
+
+import static java.awt.Color.*;
 
 public class MenuScreen implements Screen {
+    Texture img;
     MainGame game;
+    float x;
+    float y;
     public MenuScreen(MainGame game) {
         this.game = game;
     }
 
     @Override
     public void show() {
-
+        img = new Texture("background.jpg");
     }
+
+
+
 
     @Override
     public void render(float delta) {
+        // Gdx.gl.glClearColor(.1f,1,0,1);
+        Gdx.gl.glClearColor(255/255f, 255/255f, 255/255f, 1);
+
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
         game.batch.begin();
+        game.batch.draw(img, x, y);
+
+
+
         game.batch.end();
     }
 
