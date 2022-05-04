3e2293e1d9a0a7ca9a193a7c53ee7643ab954139
Max <kolxy@qq.com>
2022-05-03 21:05:29 -0700

    fix

--- core/src/com/javasupremacy/hardmode/factories/EnemyShipFactory.java
+++ core/src/com/javasupremacy/hardmode/factories/EnemyShipFactory.java
@@ -2,14 +2,7 @@ package com.javasupremacy.hardmode.factories;
 
 import com.javasupremacy.hardmode.objects.Enemy;
 import com.javasupremacy.hardmode.objects.EnemyShip;
-import com.javasupremacy.hardmode.objects.EnemyShipA;
-import com.javasupremacy.hardmode.objects.EnemyShipB;
 import org.json.simple.JSONObject;
-
-import java.lang.reflect.InvocationTargetException;
-import java.util.LinkedList;
-import java.util.List;
-import java.util.Queue;
 
 public class EnemyShipFactory implements EnemyFactory{
 
@@ -17,6 +10,7 @@ public class EnemyShipFactory implements EnemyFactory{
     public Enemy produce(JSONObject object) {
         return new EnemyShip.Builder()
                 .hp(((Long) object.get("hp")).intValue())
+                .score(((Long) object.get("reward")).intValue())
                 .texture((String) object.get("texture"))
                 .hitbox(((Long) object.get("x")).intValue(),
                         ((Long) object.get("y")).intValue(),
